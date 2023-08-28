e5cac976042c39e7dea1ed5c15d741389ee011c8
Julian So <ch.julian.so@gmail.com>
2023-08-28 19:44:01 -0400

    trying to add functions to class as methods, arrays to objects

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Write an insert and delete functions which accepts a value...
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -7,8 +9,71 @@ class Node {
 }
 
 class Tree {
-  constructor() {
-    this.root = buildTree();
+  constructor(arr) {
+    const sortedArr = this.mergeSort(this.removeDuplicates(arr));
+    console.log(typeof sortedArr);
+    this.root = this.buildTree(sortedArr);
+  }
+
+  removeDuplicates(arr) {
+    const arrNoDups = [];
+    Array.from(arr).forEach((i) => {
+      if (!arrNoDups.includes(i)) {
+        arrNoDups.push(i);
+      }
+    });
+    return arrNoDups;
+  }
+
+  // merge sort function
+  mergeSort(arr) {
+    if (arr.length < 2) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+    return this.merge(this.mergeSort(leftArr), this.mergeSort(rightArr));
+  }
+
+  merge(leftArr, rightArr) {
+    const sortedArr = [];
+    let countL = 0;
+    let countR = 0;
+    while (countL < leftArr.length && countR < rightArr.length) {
+      if (leftArr[countL] < rightArr[countR]) {
+        sortedArr.push(leftArr[countL]);
+        countL++;
+      } else {
+        sortedArr.push(rightArr[countR]);
+        countR++;
+      }
+    }
+
+    while (countL < leftArr.length) {
+      sortedArr.push(leftArr[countL]);
+      countL++;
+    }
+
+    while (countR < rightArr.length) {
+      sortedArr.push(rightArr[countR]);
+      countR++;
+    }
+    return sortedArr;
+  }
+
+  buildTree(sortedArr, start = 0, end = sortedArr.length - 1) {
+    console.log(sortedArr.length);
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+
+    let root = sortedArr[mid];
+    let node = new Node(root);
+
+    node.left = this.buildTree(sortedArr, start, mid - 1);
+    node.right = this.buildTree(sortedArr, mid + 1, end);
+
+    return node;
   }
 }
 
@@ -25,25 +90,12 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 };
 
-let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(tree);
+console.log(typeof tree);
+prettyPrint(tree.buildTree());
 
-const buildTree = (arr, start = 0, end = arr.length - 1) => {
-  if (start > end) return null;
-
-  let mid = Math.floor((start + end) / 2);
-
-  let root = arr[mid];
-  let node = new Node(root);
-
-  node.left = buildTree(arr, start, mid - 1);
-  node.right = buildTree(arr, mid + 1, end);
-  // prettyPrint(node);
-
-  return node;
-};
-
-prettyPrint(buildTree(mergeSort(removeDuplicates(arr))));
-
+// pseudocode for Binary Search Tree
 // const createBST(arr, start, end) {
 //   if (start > end) return null;
 //   let mid = (start + end) / 2;
@@ -52,49 +104,3 @@ prettyPrint(buildTree(mergeSort(removeDuplicates(arr))));
 //   root.left(createBST(arr, start, mid-1))
 //   root.right(createBST(arr, start+1, end))
 // }
-
-function removeDuplicates(arr) {
-  const arrNoDups = [];
-
-  arr.forEach((i) => {
-    if (!arrNoDups.includes(i)) {
-      arrNoDups.push(i);
-    }
-  });
-  return arrNoDups;
-}
-
-function mergeSort(arr) {
-  if (arr.length < 2) return arr;
-
-  const mid = Math.floor(arr.length / 2);
-  const leftArr = arr.slice(0, mid);
-  const rightArr = arr.slice(mid);
-  return merge(mergeSort(leftArr), mergeSort(rightArr));
-}
-
-function merge(leftArr, rightArr) {
-  const sortedArr = [];
-  let countL = 0;
-  let countR = 0;
-  while (countL < leftArr.length && countR < rightArr.length) {
-    if (leftArr[countL] < rightArr[countR]) {
-      sortedArr.push(leftArr[countL]);
-      countL++;
-    } else {
-      sortedArr.push(rightArr[countR]);
-      countR++;
-    }
-  }
-
-  while (countL < leftArr.length) {
-    sortedArr.push(leftArr[countL]);
-    countL++;
-  }
-
-  while (countR < rightArr.length) {
-    sortedArr.push(rightArr[countR]);
-    countR++;
-  }
-  return sortedArr;
-}
